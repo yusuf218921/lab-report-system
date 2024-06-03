f3e062ee1e7405a248a61e66741a61d0d141b2ce
Nemarie <yusufsonmez951@gmail.com>
2024-06-03 14:32:41 +0300

    Initial Commit

---
+++ src/main/java/com/bigbox/labreports/system/LaboratoryReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bigbox.labreports.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigbox/labreports/system/LaboratoryReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbox.labreports.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
